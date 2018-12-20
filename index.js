06c63e5bae27e4ec2bd722458d51b6208f04f9b4
Elizabeth Kosowski <elizabethkosowski@gmail.com>
2018-12-20 17:20:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ function theBeatlesPlay(arrMusicians, arrInstruments) {
   var arr = [];
   
   for (var i = 0; i < arrMusicians.length; i++) {
-    for (var j = 0; j < arrInstruments.length; j++) {
       arr.push(`${arrMusicians[i]} plays ${arrInstruments[j]}`);
-    }
   }
   return arr;
 }
