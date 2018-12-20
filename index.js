b3293e49ad609801347c8093fff80f82ec465f7c
Elizabeth Kosowski <elizabethkosowski@gmail.com>
2018-12-20 17:17:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // add solution here
+function theBeatlesPlay(arrMusicians, arrInstruments) {
+  var arr = [];
+  
+  for (var i = 0; i < arrMusicians.length; i++) {
+    for (var j = 0; j < arrInstruments.length; j++) {
+      arr.push(arrMusicians[i], arrInstruments[j])
+    }
+  }
+}
